495fcd7e458bc3b9e061588649976fa49062f273
Michael Murphy <dodgegt8@gmail.com>
2017-09-11 22:03:16 -0500

    added scripts

--- Count Game/scripts/game-es6.js
+++ Count Game/scripts/game-es6.js
@@ -1,1 +1,12 @@
 // logic for the Count Game
+
+class Game{
+	constructor(){
+	console.log('Welcome to the Count Game. Version ${this.version()}');
+	}
+	version(){
+		return '1.0.0';
+	}
+}
+//Start the Game
+var game = new Game();
